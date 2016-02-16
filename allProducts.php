a5a79f34d41d87eb40959e50a9814e138937937c
Ghada <ghada@localhost.localdomain>
2016-02-16 17:44:06 +0200

    Ghada t3bt mn sara

--- allProducts.php
+++ allProducts.php
@@ -1,16 +1,35 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<html>
 <head>
   <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
   <title>All Products</title>
   <link href="css/style.css" rel="stylesheet" type="text/css"  media="all" />
-  <link type="text/css" href="css/bootstrap.min.css" rel="stylesheet" />
+  <!--<link type="text/css" href="css/bootstrap.min.css" rel="stylesheet" />-->
 </head>
-<script src="js/jquery-1.11.2.js"></script>
-<script src="js/bootstrap.min.js" ></script> 
+    <script src="js/jquery-1.11.2.js"></script>
+<!--    <script src="js/bootstrap.min.js" ></script> -->
+    <script>
+        $(document).ready(function(){
+            var f = "true" ;
+	    $(".Availability").click(function(){
+// 		$(this).attr('clicked','clicked');
+// 		$(this).toggle();
+                if(f == "true"){
+//                      $(this).attr
+                }else{
+                
+                }
+		console.log("hi");
+	    });
+	});
+    </script>
+    <style>
+        .Availability{
+                
+        }
+    </style>
 <body>
    <div class="container form" style="width:900px ; min-height:400px ; margin:100px 200px">
-       <h2>All Users</h2><br/>
+       <h2>All Products</h2><br/>
        <span style="float:right;"><a href="addProduct.html" >add Product</a></span>
        <table class="table table-bordered myTable">
            <tr>
@@ -19,30 +38,19 @@
               <th>Image</th>
               <th>Action</th> 
            </tr>
-           <tr>
-              <td>Name</td>
-              <td>Room</td>
-              <td>Image</td>
-              <td>Action</td> 
-           </tr>
-           <tr>
-              <td>Name</td>
-              <td>Room</td>
-              <td>Image</td>
-              <td>Action</td> 
-           </tr>
-           <tr>
-              <td>Name</td>
-              <td>Room</td>
-              <td>Image</td>
-              <td>Action</td> 
-           </tr>
-           <tr>
-              <td>Name</td>
-              <td>Room</td>
-              <td>Image</td>
-              <td>Action</td> 
-           </tr>           
+           <?php 
+               require_once('tables.php') ;
+               $product = new Products() ;           
+               $res =  $product->select(); 
+               
+                for( $i =0 ; $i< count($res); $i++){
+		    echo "<tr><td>".$res[$i][1]."</td>" ;
+		    echo "<td>".$res[$i][4]." EPG</td>" ;
+		    echo "<td><img width='60px' height='60px' src='images/".$res[$i][2]."'/></td>" ; 
+		    echo "<td><button class='Availability' >Availability</button><button>DEL</button><button>Edit</button></td></tr>" ;        
+              }
+           ?>
+               
        </table>
       
    <div class="row">
--- tables.php
+++ tables.php
@@ -50,7 +50,7 @@
     }
     ////////////////////////-----------products--------------///////////////////
     class Products{
-        private $pid,$pname,$productPicture,$cid,$con;
+        private $pid,$pname,$productPicture,$cid,$price,$con;
         function __construct(){
             $this->pid=1;$this->pname='';
             $this->cid=1; $this->productPicture='';
@@ -67,11 +67,11 @@
             $this->$name = $value;
         }
         function update(){
-            $query="update Products set pname='".$this->pname."',cid='".$this->cid."',productPicture='".$this->productPicture."' where pid='".$this->pid."';";
+            $query="update Products set pname='".$this->pname."',cid='".$this->cid."',productPicture='".$this->productPicture."',price='".$this->price."' where pid='".$this->pid."';";
             mysqli_query($this->con,$query);
         }
         function insert(){
-            $query="insert into Products values(null,'".$this->pname."','".$this->productPicture."','".$this->cid."');";
+            $query="insert into Products values(null,'".$this->pname."','".$this->productPicture."','".$this->price."','".$this->cid."');";
             mysqli_query($this->con,$query);
         }
         function delete(){
