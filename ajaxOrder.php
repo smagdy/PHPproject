5311e80e62eebd3052b36dcb1ef010c2fb50e177
Sara Magdy <SaRa@localhost.localdomain>
2016-02-16 17:20:21 +0200

    :P

--- ajaxOrder.php
+++ ajaxOrder.php
@@ -3,21 +3,6 @@
     $orders=new Orders();
     $orders->uid=$_GET['uid'];
     $data=$orders->selectbykey($_GET['from'],$_GET['to']);
-    //echo $data;
-  //  echo $data[0][0];
-   /* $response = array();
-    for($i=0;$i<count($data);$i++) {
-       // echo $data[$i];
-        $x= array();
-
-        $x[0] = $data[$i][0];
-        $x[1] =$data[$i][1];
-        $x[2]= $data[$i][2];
-        $x[3] = $data[$i][3];
-        $x[4] = $data[$i][4];
-        $response[$i]=json_encode($x);
-    }
-    $response =json_encode($response);*/
     $response =json_encode($data);
     echo $response;
 
