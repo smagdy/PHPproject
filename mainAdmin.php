f0287765d6c7a76254cce5160de2c0939b9cfda4
Doaa Negm <dnegm@localhost.localdomain>
2016-02-20 20:37:15 +0200

    nav im mainuse

---
+++ mainAdmin.php
@@ -0,0 +1,30 @@
+ 
+ 
+ <div class="row">
+		<div class="col-sm-12">
+<!---------------------- start nav --------------------------------------->
+<br>
+ 
+ <div class="header navbar-fixed-top">
+	<div class="header_top">
+		    <div class="menu">
+		      <nav class="navbar navbar-default ">
+			  <ul>
+				<li class="active" ><a href="">HOME</a></li>
+				<li><a href="allProducts.html">All products</a></li>
+				<li><a href="allUsers.html">all users</a></li>
+				<li><a href="orders.html">Orders</a></li>
+				<li><a href="check.html">Checks</a></li>	
+			  </ul>
+			  <ul class="nav navbar-nav navbar-right" style="margin-right:100px" >
+				<li><img src="images/d.jpg" height="50px" width="30px"  class="img-rounded"></img></li>
+				<li ><a href="#"><span class="glyphicon "></span> Admin </a></li>				
+				<li ><a href="#"><span class="glyphicon glyphicon-log-in"></span> LogOut</a></li>				    
+			</ul>
+		      </nav>		      
+		</div>                 
+	</div>
+	</div>
+
+	</div>
+</div>
