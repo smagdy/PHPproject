632c6e31a9edd92a00c0833475e4d71e5f0470f2
Sara Magdy <SaRa@localhost.localdomain>
2016-02-22 01:34:51 +0200

    products

--- editProduct.php
+++ editProduct.php
@@ -12,7 +12,14 @@ if(isset($_GET['pid'])&&isset($_GET['edit'])){
     $product=new Products();
     $product->pid=$_GET['pid'];
 
-
+//////////////////////
+    $Product=new Products();
+    $Product->pname=$name_product;
+    $Product->productPicture=$upimage;
+    $Product->price=$price_prodect;
+    $Product->available='1';
+    $Product->cid=$category;
+    //////////////
     $product->updatecolumn();
 }
 ?>
